cb7dbdce8612ba91f780393961fa985e1882661e
inyourface3445 <trenten46@gmail.com>
2026-01-11 22:47:32 -0500

    click does not like me

--- main.py
+++ main.py
@@ -4,20 +4,23 @@ import undetected_chromedriver as uc
 from dotenv import load_dotenv
 from interactionn_prim import *
 from logger import Logger
-from runargs import Args
-from mut_elu import MutuallyExclusiveOption
-import click
-import os
+# from runargs import Args
+# from mut_elu import MutuallyExclusiveOption
+# import click
+# import os
+import sys
 
-@click.command()
-@click.option("--email", help="password for the email")
-@click.option("--target-acc", required=False, help="what account to send the money to")
-@click.option("--from-env", required=False, is_flag=True, default=True, help="use environment variables",
-              cls=MutuallyExclusiveOption,
-              mutually_exclusive=["password", "target_acc"]
-)
-def get_args(email, password, from_env, target_acc):
-    return Args(email, password, from_env, target_acc)
+# @click.command()
+# @click.option("--email", help="password for the email")
+# @click.option("--target-acc", required=False, help="what account to send the money to")
+# @click.option("--password", required=False, help="password")
+# @click.option("--from-env", required=False, is_flag=True, default=True, help="use environment variables",
+#               cls=MutuallyExclusiveOption,
+#               mutually_exclusive=["password", "target_acc"]
+# )
+# def get_args(email, password, from_env, target_acc):
+#     print("line 21")
+#     return Args(email, password, from_env, target_acc)
 
 load_dotenv()
 driver = uc.Chrome(headless=False)
@@ -25,10 +28,10 @@ driver = uc.Chrome(headless=False)
 # use the bookmarklet in the readme to get xpath, and c to copy
 
 try:
-    args = get_args()
-    password = os.getenv("PASSWORD") if args.from_env else args.password
-    email = os.getenv("SOURCE_EMAIL") if args.from_env else args.email
-    target_acc = os.getenv("TARGET_ACC") if args.from_env else args.target_acc
+    # args = get_args()
+    password = sys.argv[1]
+    email = sys.argv[2]
+    target_acc = sys.argv[3]
 
     logger = Logger(True)
 
@@ -43,6 +46,3 @@ try:
     print("done")
 except Exception as e:
     print(f"An error occurred: {e}")
-
-finally:
-    driver.quit()
